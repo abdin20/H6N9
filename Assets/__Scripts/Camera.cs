9714ecd0ed3613cb79e6fe0273ab23182df33209
naymul <naymul@hotmail.ca>
2020-03-18 21:43:06 -0400

    added background and moving camera

---
+++ Assets/__Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //set the offset of the camera only on the x axis
+        offset = new Vector3(transform.position.x - player.transform.position.x, transform.position.y,transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+        //add the offset every frame so it follows the player
+        transform.position = new Vector3(player.transform.position.x + offset.x, offset.y, transform.position.z);
+    }
+}
