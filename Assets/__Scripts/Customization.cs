d71b223ead98ad2c99be4c093ec013228aeb508e
Edward <46017382+echang49@users.noreply.github.com>
2020-03-19 22:58:59 -0400

    Character Customization Script

---
+++ Assets/__Scripts/Customization.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customization : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Awake()
+    {
+        Resume();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else { 
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    /*void OpenMenu()
+    {
+
+    }
+
+    void CloseMenu()
+    {
+
+    }
+
+    void ChangeCharacter()
+    {
+
+    }
+
+    void ChangeWeapon()
+    {
+
+    }*/
+
+}
+
+
+//someone presses escape
+//overlay menu
+//CHOICES(Change hair, change shirt, change weapon, close menu)
+//if change hair, take input of left click or right click
+
+ //seperate classes for weapons?
