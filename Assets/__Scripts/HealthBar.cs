aefbe12852a9d57310a1c5dab244ef463794fb24
Riley <roverlan@uwo.ca>
2020-03-19 21:32:50 -0400

    added health bar

---
+++ Assets/__Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealthBar : MonoBehaviour
+{
+    public Text healthText;
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setCountText();
+    }
+    public void setCountText(){
+        healthText.text = "Health left:" +health.ToString();
+        
+    }
+}
