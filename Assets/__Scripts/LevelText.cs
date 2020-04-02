5822d4c85b882206fe34baed85a6185db51ae568
Riley <roverlan@uwo.ca>
2020-04-02 18:55:01 -0400

    added level text display

---
+++ Assets/__Scripts/LevelText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelText : MonoBehaviour
+{
+    public Text levelText;
+    public GameObject player;
+    public string playerLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setLevelText();
+    }
+    public void setLevelText()
+    {
+        if(SceneManager.GetActiveScene().name == "LevelOneScene"){
+            levelText.text = "Current Level: Level 1";
+
+        }
+        else{
+            levelText.text = "Current Level: Level 2";
+        }
+        
+    }
+}
